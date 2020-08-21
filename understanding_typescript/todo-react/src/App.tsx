8ebab20db33b32237edc2b0ea05ea27583256ad3
Bailee Dastugue <bailee.dast@gmail.com>
2020-08-21 10:24:04 -0500

    managing state with tsx and useState hooks

--- understanding_typescript/todo-react/src/App.tsx
+++ understanding_typescript/todo-react/src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import {v4 as uuidv4} from 'uuid';
+import {Todo } from './todo.model';
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [...prevTodos, 
+    {id: uuidv4(), text: text}]);
+  };
+
   return (
     <div className="App">
-      Aw yeah, TypeScript
+      <NewTodo onAddTodo={todoAddHandler}/>
+      <TodoList items={todos}/>
     </div>
   );
 }
