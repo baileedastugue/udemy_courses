7bc2629081b5b13214b93b717b32c5acd70e7a10
Bailee Dastugue <bailee.dast@gmail.com>
2020-08-27 12:02:20 -0500

    covered advanced types, incl typecasting and type guards

---
+++ understanding_typescript/06_advancedTypes/src/app.ts
@@ -0,0 +1,181 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+}
+
+type Employee = {
+  name: string;
+  startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+
+// interface ElevatedEmployee extends Admin, Employee {};
+
+
+const e1: ElevatedEmployee = {
+  name: 'Bailee',
+  privileges: ['front-end'],
+  startDate: new Date() 
+}
+
+// ===========================
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+// function add(a: Combinable, b: Combinable) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+
+// ===========================
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo (emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({name: 'Jack', startDate: new Date()});
+
+// ===========================
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...')
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount)
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+// ===========================
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch(animal.type) {
+    case 'bird': 
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log('Moving with speed: ' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 500});
+
+// ===========================
+
+const paragraph = document.getElementById('message-output');
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+// const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+const userInputElement = document.getElementById('user-input');
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there';
+}
+
+// ===========================
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character' }
+  // id: string;  
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Must contain a number'
+}
+
+// ===========================
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add(1, 5);
+ 
+// ===========================
+
+const fetchUserData = {
+  id: 'u1',
+  name: 'Max',
+  job: {
+    title: 'CEO',
+    description: 'My own company'
+  }
+}
+
+// console.log(fetchUserData.job && fetchUserData.job.title);
+console.log(fetchUserData?.job?.title);
+
+// ===========================
+
+const userInput = '';
+
+const storedData = userInput || 'DEFAULT';
+
+console.log(storedData);
