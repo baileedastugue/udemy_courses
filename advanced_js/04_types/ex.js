35cbed4ff704dceade33b98a70e31b6ed615433e
Bailee Dastugue <bailee.dast@gmail.com>
2020-09-09 11:49:38 -0500

    finished section four types in js

---
+++ advanced_js/04_types/ex.js
@@ -0,0 +1,26 @@
+const number = 100;
+const string = 'Jay';
+let obj1 = {
+     value: 'a',
+};
+let obj2 = {
+     value: 'b',
+};
+let obj3 = obj2;
+
+function change(number, string, obj1, obj2) {
+     number = number * 10;
+     string = 'Pete';
+     obj1 = obj2;
+     obj2.value = 'c';
+}
+
+change(number, string, obj1, obj2);
+
+//Guess the outputs here before you run the code:
+console.log(number);
+// 1000 --> actually 100
+console.log(string);
+// Pete --> actually Jay
+console.log(obj1.value);
+// c --> actually a
---
+++ advanced_js/04_types/jstypes.js
@@ -0,0 +1,7 @@
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof 'to be or not to be');
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Symbol('just me'));
+console.log(typeof {});
